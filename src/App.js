03c1118c1418cfc90417516a502a391701efeafb
saikat1095 <mdabuthaersaikat@gmail.com>
2021-05-30 22:38:29 +0600

    Nothing done yet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Landing from "./screen/Landing";
 import SingleCategory from "./screen/SingleCategory";
