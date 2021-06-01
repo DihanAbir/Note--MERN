6911ebf1c1607a19c9aa052c240d55e499b9ba74
NAHID MURAD ABIR <nahid.muradabir@gmail.com>
2021-06-02 01:00:29 +0600

    done all backend

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const connectDb = require("./config/db");
 const errorHandler = require("./middleware/error");
 
 // Load env vars
-dotenv.config({ path: "./backend/config/.env" });
+dotenv.config({ path: "./config/.env" });
 
 // Connect to the database
 connectDb();
@@ -23,6 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
+app.use(cors());
 
 // Mounting the routers
 app.use("/api/v1/note", note);
