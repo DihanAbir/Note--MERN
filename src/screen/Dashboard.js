a49a4b68b00382b8744e7bb122c2a5d7653792f7
saikat1095 <mdabuthaersaikat@gmail.com>
2021-06-08 17:44:50 +0600

    done

--- src/screen/Dashboard.js
+++ src/screen/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../Mycss/Dashboard.css'
 import profile from '../Assets/dashboard/Ellipse 1.png';
 import backlogo from '../Assets/dashboard/fi-rr-sign-out 1.png';
+import object from '../Assets/dashboard/OBJECT 1.png'
 
 const Dashboard = () => {
     return (
@@ -11,13 +12,22 @@ const Dashboard = () => {
 
 
             <div className="profile">
-                <div>
+                <div className="top-profile">
                     <span>logout</span>
-                    <span><img src={backlogo} alt="" /></span>
+                    <span><img className="image" src={backlogo} alt="" /></span>
                 </div>
                 <img src={profile} alt="" />
                 <div className="name">Sadia Tasnim</div>
                 <div className="status">Student</div>
+            </div>
+
+
+            <div className="welcome-back">
+                <ul>
+                    <li><img className="" src={object}  alt="" /></li>
+                    <li><p>Welcome back Anna!</p></li>
+                    <li><p>You’ve learned 80% of your goal this week!</p></li>
+                </ul>
             </div>
 
         </div>
